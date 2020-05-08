100035898a98c44a2d48c2067a8bc92b126c4054
WalkOrRun <rettuu1@hotmail.com>
2020-05-08 00:04:40 -0500

    4 files modified

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormBuilder, Validators} from '@angular/forms'
 import {HttpClientModule} from '@angular/common/http';
 import {AccountService} from '../account.service';
 import { Account} from '../Account';
+import {Router } from '@angular/router';
+export type RouteTo = 'login';
+
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.component.html',
@@ -14,14 +17,25 @@ export class CreateAccountComponent implements OnInit {
   password:string;
   repassword:string;
   message:string;
-  
-  constructor(private accService: AccountService) { }
+  routing : RouteTo;
 
-  ngOnInit() {
+  toggleEditor(type: RouteTo) {
+    this.routing = type;
   }
+  get showLogin() {
+    return this.routing === 'login';
+  }
+
+  constructor(private accService: AccountService, private router: Router) { }
+  ngOnInit() {
+  } 
+  toLogin(){
+    this.router.navigate(['/login']);
+  }
+
   addNewAccount(){
     if(this.password!=this.repassword){
-      this.message='password and Retyped password have to be the same';
+      this.message='password and Re-typed password have to be the same';
       this.password = '';
       this.repassword = '';
       return;
@@ -35,7 +49,8 @@ export class CreateAccountComponent implements OnInit {
       this.name='';
       this.password='';
       this.repassword='';
-      this.message='New Account Successfully Created!';
+      this.message='New Account Successfully Created! Will redirect to login page in 4 seconds';   
+      setTimeout(()=>{ this.router.navigate(['/login']); }, 4000)
       
     });
   }
